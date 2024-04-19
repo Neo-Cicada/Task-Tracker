7e6b3a8f1226ef5e1766707ca875753d22951d21
Neo <lordneobarnachea@gmail.com>
2024-04-19 22:28:06 +0800

    switched to Type script so I can use aceternity UI

---
+++ airdroptracker/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { MacbookScroll } from "@/components/ui/macbook-scroll";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="border border-red-500">
+      <MacbookScroll/>
+    </main>
+  );
+}
--- tasktracker/src/app/(main)/airdrops/page.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Airdrops() {
-  return (
-    <div>Airdrops</div>
-  )
-}
