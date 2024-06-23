98a3311db0cef1f679ae5e4cea9498f9f57a2c77
Neo <lordneobarnachea@gmail.com>
2024-06-23 10:29:30 +0800

    add vercel analytics

--- airdroptracker/src/app/(main)/layout.tsx
+++ airdroptracker/src/app/(main)/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
 import {NavbarDemo} from "@/components/Header";
-
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -21,6 +21,7 @@ export default function RootLayout({
         <NavbarDemo/>
         {children}
         </body>
+        <Analytics/>
     </html>
   );
 }
