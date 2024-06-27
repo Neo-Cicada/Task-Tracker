416eb23b1a37c6424450aeab11f196a649fb2eb8
Neo <lordneobarnachea@gmail.com>
2024-06-27 20:37:41 +0800

    add todo comment

--- airdroptracker/src/components/mobile/MobileMenu.tsx
+++ airdroptracker/src/components/mobile/MobileMenu.tsx
@@ -6,10 +6,10 @@ interface MobileMenuProps {
     onClose: () => void;
 }
 
-
+//TODO: reset the isOpen to false if the size of the screen changes or moves
 const MobileMenu: React.FC<MobileMenuProps> = ({ isOpen, onClose }) => {
     return (
-        <div className='fixed left-0 z-20 w-full bg-white border border-red-500 h-1/2 top-25'>
+        <div className='fixed left-0 z-20 w-full bg-white border border-red-500 h-1/2 top-25 md:hidden'>
             <div className='flex flex-col items-center justify-around h-full'>
                 <Link
                     className="mb-4 text-lg font-extrabold text-black" href="/"
