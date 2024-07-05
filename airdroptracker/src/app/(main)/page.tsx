e7fb42c7d224d8b5b73f764944a1c6fc0efaa262
Neo <lordneobarnachea@gmail.com>
2024-07-05 22:29:33 +0800

    add .env

--- airdroptracker/src/app/(main)/page.tsx
+++ airdroptracker/src/app/(main)/page.tsx
@@ -6,14 +6,24 @@ export default function Home() {
       <main className="md:flex h-[90vh]">
         <div className="flex items-center justify-center border h-1/2 md:h-full md:w-1/2">
           <div className="border border-gray-600 w-[80%] h-[80%] rounded-md">
-            qweqwe
+            Intro to the website 
           </div>
         </div>
         <div className="flex flex-col justify-center gap-10 p-12 border h-1/2 md:h-full md:w-1/2">
           {/* btc, eth, sol, bnb price */}
-          <div className="border border-gray-600 rounded-md h-36 ">wqeqwe</div>
-          <div className="border border-gray-600 rounded-md h-36 ">qweqwe</div>
-          <div className="border border-gray-600 rounded-md h-36 ">qweqwe</div>
+          <div className="border border-gray-600 rounded-md h-36 ">
+            <p>BTC</p>
+            <p>ETH</p>
+            <p>BNB</p>
+            <p>XRP</p>
+            <p>TON</p>
+          </div>
+          <div className="border border-gray-600 rounded-md h-36 ">
+            newly added airdrop project
+          </div>
+          <div className="border border-gray-600 rounded-md h-36 ">
+            crypto ads position
+          </div>
         </div>
       </main>
     </>
