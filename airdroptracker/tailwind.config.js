1d05bc9c6442e9917e95d64c053425e96869c08f
Neo <lordneobarnachea@gmail.com>
2024-07-12 11:15:23 +0800

    made the default bg black and text white

--- airdroptracker/tailwind.config.js
+++ airdroptracker/tailwind.config.js
@@ -1,56 +1,51 @@
+import type { Config } from "tailwindcss";
 const defaultTheme = require("tailwindcss/defaultTheme");
+
 const colors = require("tailwindcss/colors");
 const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors: {
-        customBlue: 'rgb(6, 182, 212)',
-      },
-      // If you want to extend the opacity scale
-      opacity: {
-        'custom-opac': 'var(--tw-bg-opacity)',
-      },
-      animation: {
-        spotlight: "spotlight 2s ease .75s 1 forwards",
-      },
-      keyframes: {
-        spotlight: {
-          "0%": {
-            opacity: 0,
-            transform: "translate(-72%, -62%) scale(0.5)",
-          },
-          "100%": {
-            opacity: 1,
-            transform: "translate(-50%,-40%) scale(1)",
-          },
-        },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [addVariablesForColors],
-}
+  plugins: [
+    addVariablesForColors,
+    function ({ addBase }) {
+      addBase({
+        'body': {
+          backgroundColor: '#000000', // Black background
+          color: '#ffffff', // White text
+        },
+      });
+    },
+  ],
+};
 
-// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
 function addVariablesForColors({ addBase, theme }: any) {
   let allColors = flattenColorPalette(theme("colors"));
   let newVars = Object.fromEntries(
     Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
   );
- 
+
   addBase({
     ":root": newVars,
   });
 }
+
+export default config;
