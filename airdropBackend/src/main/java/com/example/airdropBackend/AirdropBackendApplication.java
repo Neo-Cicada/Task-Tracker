1d8dd81adc7b1764bae519e962ab7315bc4ac58a
Neo <lordneobarnachea@gmail.com>
2024-07-31 18:06:37 +0800

    add spring boot build

---
+++ airdropBackend/src/main/java/com/example/airdropBackend/AirdropBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.airdropBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirdropBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirdropBackendApplication.class, args);
+	}
+
+}
---
+++ airdropBackend/src/test/java/com/example/airdropBackend/AirdropBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airdropBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirdropBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
