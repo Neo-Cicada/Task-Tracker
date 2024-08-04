8bf8c2834b3554507e887002bff4e3a195a89bc4
Neo <lordneobarnachea@gmail.com>
2024-08-04 22:30:10 +0800

    fix something

---
+++ airdroptracker/src/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Image from 'next/image';
+export default function Login() {
+
+  return (
+
+    <div className='border border-red-500'>
+      test
+    </div>
+  )
+}
--- airdroptracker/src/app/(main)/airdrops/page.tsx
+++ airdroptracker/src/app/(main)/airdrops/page.tsx
@@ -2,8 +2,86 @@
 import React, { useState } from 'react'
 import Image from 'next/image';
 import PopUp from '@/components/PopUp';
+import AirdropInfo from '@/components/AirdropInfo';
 export default function Airdrops() {
+  const [hoveredData, setHoveredData] = useState<any>(null);
+
+  const dummyData = [
+    {
+      "id": "1",
+      "name": "neo",
+      "link": "www.facebook.com",
+      "description": "lorem epsum",
+      "platform": "Telegram",
+      "network": "Solana",
+      "requirements": ["This1", "This2", "This3"],
+      "socials": {
+        "twitter": "twitter.com",
+        "discord": "discord.com",
+        "telegram": "telegram.com"
+      }
+    },
+    {
+      "id": "2",
+      "name": "lord",
+      "link": "www.facebook.com",
+      "description": "lorem epsum dorem moren awdawd tangina mo dutrer",
+      "platform": "Telegram",
+      "network": "Solana",
+      "requirements": ["This1", "This2", "This3"],
+      "socials": {
+        "twitter": "twitter.com",
+        "discord": "discord.com",
+        "telegram": "telegram.com"
+      }
+    },
+    {
+      "id": "3",
+      "name": "the",
+      "link": "www.facebook.com",
+      "description": "lorem epsum dorem moren awdawd tangina mo dutrer",
+      "platform": "Galxe",
+      "network": "Avax",
+      "requirements": ["This1", "This2", "This3"],
+      "socials": {
+        "twitter": "twitter.com",
+        "discord": "discord.com",
+        "telegram": "telegram.com"
+      }
+
+    },
+    {
+      "id": "4",
+      "name": "great",
+      "link": "www.facebook.com",
+      "description": "lorem epsum dorem moren awdawd tangina mo dutrer",
+      "platform": "Telegram",
+      "network": "Solana",
+      "requirements": ["This1", "This2", "This3"],
+      "socials": {
+        "twitter": "twitter.com",
+        "discord": "discord.com",
+        "telegram": "telegram.com"
+      }
+
+    }
+  ]
   const [isPopUp, setPopUp] = useState<boolean>(false);
+
+  const componentData = dummyData.map(item => (
+    <div
+      key={item.id}
+      className="flex items-center justify-around h-16 mx-24 border border-gray-600 rounded-md cursor-pointer"
+      onClick={() => {
+        setPopUp(!isPopUp);
+        setHoveredData(item);
+
+      }}
+    >
+      <div >{item.name}</div>
+      <div >{item.network}</div>
+    </div>
+  ))
   return (
     <div className='flex flex-col h-[90vh] p-9 gap-4'>
       {/* List of project,names, links, funding, steps, requirements, date started,  TGE */}
@@ -20,28 +98,18 @@ export default function Airdrops() {
         </div>
       </div>
       <div className='flex flex-col gap-3 '>
-        <div
-          className="flex items-center justify-around h-16 mx-24 border border-gray-600 rounded-md cursor-pointer"
-          onClick={()=>setPopUp(!isPopUp)}
-        >
-          <div >name</div>
-          <div >name</div>
-        </div>
-        <div
-          className="flex items-center justify-around h-16 mx-24 border border-gray-600 rounded-md cursor-pointer"
-        >
-          <div >name</div>
-          <div >name</div>
-        </div>
-        <div
-          className="flex items-center justify-around h-16 mx-24 border border-gray-600 rounded-md cursor-pointer"
-        >
-          <div >name</div>
-          <div >name</div>
-        </div>
+        {componentData}
       </div>
       <PopUp popUp={isPopUp} onClose={() => setPopUp(false)}>
-        hello
+        <div className='w-[70vw] h-[80vh] flex flex-col justify-start gap-10'>
+          <div className='flex items-center justify-center'>
+            <p className='text-3xl font-extrabold'>{hoveredData && hoveredData.name}</p>
+          </div>
+          <p>{hoveredData && hoveredData.description}</p>
+          <p>{hoveredData && hoveredData.link}</p>
+          <p>{hoveredData && hoveredData.platform}</p>
+          {hoveredData && hoveredData.requirements.map((item: string) => <p key={item}> {item}</p>)}
+        </div>
       </PopUp>
     </div>
   )
